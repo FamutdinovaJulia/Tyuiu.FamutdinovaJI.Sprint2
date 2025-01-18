9ab0db005b7acfea06fe109685b642d01e634084
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-18 17:00:07 +0500

    task0

---
+++ Tyuiu.FamutdinovaJI.Sprint2.Task0.V30/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint2.Task0.V30.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint2.Task0.V30;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Фамутдинова Ю. И. | ИИПб-24-2";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #30                                                             *");
+        Console.WriteLine("* Выполнила: Фамутдинова Юлия Ильфатовна | ИИПб-24-2                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Напишите программу, которая вычисляет площадь треугольника, если        *");
+        Console.WriteLine("* известны координаты его углов.                                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 95;
+
+        int y = 1735;
+
+        bool[] res = ds.GetCompareOperations(x, y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+    }
+}

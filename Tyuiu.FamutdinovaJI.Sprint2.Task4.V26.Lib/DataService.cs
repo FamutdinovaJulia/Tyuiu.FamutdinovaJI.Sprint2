d8a872a82aab5ca3f47cf49b9438b51a0ed048e8
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-20 02:54:39 +0500

    Task4

---
+++ Tyuiu.FamutdinovaJI.Sprint2.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.FamutdinovaJI.Sprint2.Task4.V26.Lib
+{
+    public class DataService : ISprint2Task4V26
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = 0;
+            double z1 = 0;
+
+            if ((x - 2)<(y / 2))
+            {
+                z1 = 10 + (2 / (x * x));
+                z = Math.Pow(z1, y);
+            }
+            else
+            {
+                z = x * x - (1 / y);
+            }
+            return Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.FamutdinovaJI.Sprint2.Task4.V26/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint2.Task4.V26.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint2.Task4.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* Введите значение переменной Х:                                          *");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите значение переменной Y:                                          *");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x,y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
